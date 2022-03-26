b0717e351e497beca99385746b721bf311534467
Homayoon <homayoonalimohammadi@gmail.com>
2022-03-26 13:30:03 +0430

    Update BST

--- Binary Search Tree.py
+++ Binary Search Tree.py
@@ -4,15 +4,19 @@ class Node:
         self.left = left
         self.right = right
 
+    
     def __str__(self):
-        return f'Value: {self.value}, Left: {self.left}, Right: {self.right}'
+        return f'''Value: {self.value}
+    \tLeft: {self.left}
+    \tRight: {self.right}
+        '''
 
 
 class BinarySearchTree:
     def __init__(self, value):
         new_node = Node(value)
         self.root = new_node
-        self.depth = 1
+        self.max_depth = 1
 
     def insert(self, value):
         new_node = Node(value)
@@ -31,6 +35,19 @@ class BinarySearchTree:
                     break
                 else:
                     lookup_node = lookup_node.right
-                    
-    
+        self.max_depth += 1
+        return self
 
+    def __str__(self):
+        return str(self.root)
+
+
+
+
+bst = BinarySearchTree(10)
+bst.insert(20)
+bst.insert(9)
+bst.insert(5)
+bst.insert(9.5)
+bst.insert(25)
+print(bst)
