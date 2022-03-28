c05d61dc5404dff966ac3782363e67f246c70616
Homayoon <homayoonalimohammadi@gmail.com>
2022-03-28 16:38:29 +0430

    Fix QuickSort

--- Quick Sort.py
+++ Quick Sort.py
@@ -2,21 +2,25 @@ from random import sample, shuffle
 from time import time
 
 
-def QuickSort(arr, pivot_index=0):
-    
-    if len(arr) == 1:
+def QuickSort(arr):
+
+    if len(arr) <= 1:
         return arr
 
-    left = arr[:pivot_index]
-    right = arr[pivot_index+1:]
-    left_sorted_by_pivot, new_pivot_index = SortByPivot(left)
-    right_sorted_by_pivot, new_pivot_index = SortByPivot(right)
-    return left + [arr[pivot_index]] + right
-    
+    arr_sorted, pivot_index = SortByPivot(arr)
+    left = arr_sorted[:pivot_index]
+    right = arr_sorted[pivot_index+1:]
 
+    left_sorted = QuickSort(left)
+    right_sorted = QuickSort(right)
+
+    return left_sorted + [arr_sorted[pivot_index]] + right_sorted
 
 
 def SortByPivot(arr):
+    if len(arr) == 0:
+        return [], 0
+
     pivot = arr[0]
     swap_idx = 0
     for i in range(1, len(arr)):
@@ -28,5 +32,13 @@ def SortByPivot(arr):
     return arr, swap_idx
 
 
-l = [4,6,1,7,3,2,5]
-print(SortByPivot(l))
+arr = sample(range(1,100000), 10000)
+shuffle(arr)
+
+t0 = time()
+generic_sort = sorted(arr)
+print('generic sort:', time() - t0)
+t0 = time()
+quick_sort = QuickSort(arr)
+print('quick sort:', time() - t0)
+print(generic_sort == quick_sort)
