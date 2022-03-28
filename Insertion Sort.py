1548a17f5ccadbdbc1ac57bbacd79785d1c7c439
Homayoon <homayoonalimohammadi@gmail.com>
2022-03-28 09:35:07 +0430

    Add Selection Sort

---
+++ Insertion Sort.py
@@ -0,0 +1,21 @@
+from random import sample, shuffle
+from time import time
+
+
+def InsertionSort(arr):
+    for i in range(1, len(arr)):
+        while arr[i] < arr[i-1] and i > 0:
+            arr[i], arr[i-1] = arr[i-1], arr[i]
+            i -= 1
+    return arr
+
+
+# arr = sample(range(1,100000), 10000)
+# shuffle(arr)
+# t0 = time()
+# generic_sort = sorted(arr)
+# print('generic sort:', time() - t0)
+# t0 = time()
+# insertion_sort = InsertionSort(arr)
+# print('selection sort:', time() - t0)
+# print(generic_sort == insertion_sort)
---
+++ Selection Sort.py
@@ -0,0 +1,24 @@
+from random import sample, shuffle
+from time import time
+
+
+def SelectionSort(arr):
+    for i in range(len(arr)):
+        min_index = i
+        for j in range(i+1, len(arr)):
+            if arr[j] < arr[min_index]:
+                min_index = j
+        if min_index != i:
+            arr[i], arr[min_index] = arr[min_index], arr[i]
+
+    return arr
+
+arr = sample(range(1,100000), 10000)
+shuffle(arr)
+t0 = time()
+generic_sort = sorted(arr)
+print('generic sort:', time() - t0)
+t0 = time()
+selection_sort = SelectionSort(arr)
+print('selection sort:', time() - t0)
+print(generic_sort == selection_sort)
